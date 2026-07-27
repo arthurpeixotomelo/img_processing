2ec2d268273c1a173bc64dbc29757abce6fd1f76
Arthur Peixoto Melo <arthurpeixotomelo@gmail.com>
2026-07-26 23:36:48 -0300

    update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf7538170ce68b261d44c015c4cef2202b384bf296d1a72ec9ab11fdcbee29cc
-size 1416
+oid sha256:ffdaf12380dddd4c7e03d6f4ea114a3117f9dba706083174de989df8141d0cc4
+size 1504
--- etl.py
+++ etl.py
@@ -12,17 +12,6 @@ __generated_with = "0.23.14"
 app = marimo.App()
 
 
-@app.cell(hide_code=True)
-def _(mo):
-    mo.md(r"""
-    1. cleanup the username categories to the 5 brands for each type in the dataset.
-    2. review each of the notebooks and make sure the code is simplified.
-    3. adjust the plotting functions and generate the plots for the final paper draft.
-    4. review the whole draft and put the plotting into it with full, type, brand groupings.
-    """)
-    return
-
-
 @app.cell
 def _():
     import polars as pl
@@ -33,14 +22,12 @@ def _():
 
 @app.cell
 def _(pl):
-    df = (
-      pl.read_csv("image_analysis_full.csv")
-      .with_columns(
-        pl.when(pl.col("color_category").is_in(["White", "Black", "Gray"]))
+    df = pl.read_csv("image_analysis_full.csv").with_columns(
+        pl
+        .when(pl.col("color_category").is_in(["White", "Black", "Gray"]))
         .then(pl.lit("achromatic"))
         .otherwise(pl.lit("chromatic"))
         .alias("primary_color_category")
-      )
     )
     df
     return (df,)
